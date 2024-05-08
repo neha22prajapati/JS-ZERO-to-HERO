9cb7faf7fe4f569de716a62c261d9e18be61872d
Neha Prajapati <nehaprajapati@Nehas-MacBook-Air.local>
2024-05-08 23:10:10 +0530

    added filter functionaly with show/hide error message

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const url = "person.json";
 let ul = document.querySelector("ul");
-let input = document.getElementById("filterbyPerson");
-input.onchange = function () {
-  filterbyPerson(e);
-};
+let p = document.getElementById("error");
 
 const getData = async () => {
   const res = await fetch(url);
@@ -25,8 +22,33 @@ const displayData = async () => {
 
 displayData();
 
-filterbyPerson = async (e) => {
+filterPersonData = async () => {
+  let input = document.getElementById("filterbyPerson");
+  if (input.value.length < 3) {
+    if (input.value.length === 0) {
+      p.style.display = "none";
+      ul.style.display = "block";
+      displayData();
+    }
+    return;
+  }
+
   const list = await getData();
-  console.log(list);
-  console.log(e);
+  let value = list
+    .filter((li) => li.firstName.toUpperCase() === input.value.toUpperCase())
+    .map((object) => {
+      const { firstName, lastName } = object;
+      return `<li>${firstName} ${lastName}</li>`;
+    })
+    .join("");
+
+  if (value) {
+    p.style.display = "none";
+    ul.innerHTML = value;
+    ul.style.display = "block";
+  } else {
+    ul.style.display = "none";
+    p.innerHTML = "Oops! No data present with this name";
+    p.style.display = "block";
+  }
 };
